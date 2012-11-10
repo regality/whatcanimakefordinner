ed3374b98636e9480fd75716e171fa28cf3ec25f
Adam Blackburn <regality@gmail.com>
2012-11-10 13:56:48 -0700

    asdf

--- public/js/main.js
+++ public/js/main.js
@@ -9757,48 +9757,6 @@ return jQuery;
 
 });
 
-require.define("/client/typeahead.js",function(require,module,exports,__dirname,__filename,process,global){require('./bootstrap.js');
-
-$("input#search").typeahead({
-  source: searchIngredient,
-  items: 10,
-  minLength: 1,
-  matcher: function() {
-    return true;
-  },
-  highlighter: function(item) {
-    return item.name;
-  },
-  sorter: function(items) {
-    return items;
-  },
-  updater: function(item) {
-    item = JSON.parse(item);
-    $("#ingredient-list").append(item.name + '<br/>')
-    return '';
-  }
-});
-
-function searchIngredient(query, cb) {
-  $.ajax({
-    url: '/search/ingredient',
-    data: {
-      q: query
-    },
-    success: function(data) {
-      //var results = data.results.map(function(v) { return v.name; });
-      for (var i = 0; i < data.results.length; ++i) {
-        data.results[i].toString = function() {
-          return JSON.stringify(this);
-        }
-      }
-      cb(data.results);
-    }
-  });
-}
-
-});
-
 require.define("/client/bootstrap.js",function(require,module,exports,__dirname,__filename,process,global){/* ===================================================
  * bootstrap-transition.js v2.2.1
  * http://twitter.github.com/bootstrap/javascript.html#transitions
@@ -11824,6 +11782,48 @@ require.define("/client/bootstrap.js",function(require,module,exports,__dirname,
 
 
 }(window.jQuery);
+});
+
+require.define("/client/typeahead.js",function(require,module,exports,__dirname,__filename,process,global){require('./bootstrap.js');
+
+$("input#search").typeahead({
+  source: searchIngredient,
+  items: 10,
+  minLength: 1,
+  matcher: function() {
+    return true;
+  },
+  highlighter: function(item) {
+    return item.name;
+  },
+  sorter: function(items) {
+    return items;
+  },
+  updater: function(item) {
+    item = JSON.parse(item);
+    $("#ingredient-list").append(item.name + '<br/>')
+    return '';
+  }
+});
+
+function searchIngredient(query, cb) {
+  $.ajax({
+    url: '/search/ingredient',
+    data: {
+      q: query
+    },
+    success: function(data) {
+      //var results = data.results.map(function(v) { return v.name; });
+      for (var i = 0; i < data.results.length; ++i) {
+        data.results[i].toString = function() {
+          return JSON.stringify(this);
+        }
+      }
+      cb(data.results);
+    }
+  });
+}
+
 });
 
 require.alias("jquery-browserify", "/node_modules/jquery");
